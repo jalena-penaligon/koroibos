23c3ec5aeb8717d28e30c435d29dc7121f6887ac
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-30 09:41:06 -0600

    Customizes as_json options and defines total_medals

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -4,4 +4,19 @@ class Olympian < ApplicationRecord
   has_many :events, through: :olympian_events
   validates :name, presence: true, uniqueness: true
   validates_presence_of :age, :sex, :weight, :team, :height
+
+  def as_json options={}
+    {
+      name: name,
+      team: team,
+      age: age,
+      sport: Sport.find(sport_id).name,
+      total_medals_won: total_medals
+    }
+  end
+
+  def total_medals
+    self.olympian_events.where.not(medal: "NA").count
+  end
+
 end
