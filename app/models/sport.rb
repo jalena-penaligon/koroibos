ed13678df7604551f8bf3b4726aa2b5fac10f748
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-30 17:08:19 -0600

    Customizes sport as_json method

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -2,4 +2,11 @@ class Sport < ApplicationRecord
   has_many :olympians
   has_many :events
   validates :name, presence: true, uniqueness: true
+
+  def as_json options={}
+    {
+      name: name,
+      events: self.events.pluck(:name)
+    }
+  end
 end
