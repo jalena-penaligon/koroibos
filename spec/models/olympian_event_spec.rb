91c36e39d82a3c226e913b6371797eb9abe5294c
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-30 08:05:02 -0600

    Creates olympian_event model

--- spec/models/olympian_event_spec.rb
+++ spec/models/olympian_event_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe OlympianEvent, type: :model do
   describe 'validations' do
-    it { should validate_presence_of :olympian_id }
-    it { should validate_presence_of :event_id }
     it { should validate_presence_of :medal }
   end
 
