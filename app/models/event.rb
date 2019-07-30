49799a535b9ae1d5c7613508124c26095f18995d
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-30 08:07:29 -0600

    Adds relationships to event and olympian for olympian_events joins table

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :sport
+  has_many :olympian_events
+  has_many :olympians, through: :olympian_events
   validates :name, presence: true, uniqueness: true
 end
