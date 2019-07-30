ab7a91b57c56c30ef316f364940defd1b2ebe84a
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-30 08:36:54 -0600

    Creates dataset and csv import within seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+OlympianEvent.destroy_all
+Olympian.destroy_all
+Event.destroy_all
+Sport.destroy_all
+
+olympians = {}
+
+options = {headers: true, header_converters: :symbol}
+
+CSV.foreach('./db/olympians.csv', options) do |row|
+  olympians = row.to_h
+  sport = Sport.find_or_create_by(name: olympians[:sport])
+  olympian = sport.olympians.create(name: olympians[:name],
+                                    sex: olympians[:sex],
+                                    age: olympians[:age],
+                                    height: olympians[:height],
+                                    weight: olympians[:weight],
+                                    team: olympians[:team])
+  event = sport.events.find_or_create_by(name: olympians[:event])
+  olympian_event = OlympianEvent.create(olympian_id: olympian.id, event_id: event.id, medal: olympians[:medal])
+end
