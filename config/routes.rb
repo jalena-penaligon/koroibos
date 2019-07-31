0ec9e0406cd8a979d625c132e87b22e5c5034c25
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-31 01:28:12 -0600

    Adds most losing team route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
       get '/olympian_stats', to: 'olympian_stats#show'
       get '/events', to: 'events#index'
       get '/events/:id/medalists', to: 'events/medalists#show'
-      get '/most_winning_team', to: 'most_winning#show' 
+      get '/most_winning_team', to: 'most_winning#show'
+      get '/most_losing_team', to: 'most_losing#show'
     end
   end
 end
