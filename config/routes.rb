1334d467d0456347a0fb0339818b4988161a9f93
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-31 01:07:22 -0600

    Creates route for api/v1/most_winning_team

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
       get '/olympians', to: 'olympians#index'
       get '/olympian_stats', to: 'olympian_stats#show'
       get '/events', to: 'events#index'
-      get '/events/:id/medalists', to: 'events/medalists#show' 
+      get '/events/:id/medalists', to: 'events/medalists#show'
+      get '/most_winning_team', to: 'most_winning#show' 
     end
   end
 end
